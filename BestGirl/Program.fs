c2a41255da250131c4af2502b46bb938c2c88af8
Devin <devin-lynch@uiowa.edu>
2018-02-17 18:12:53 -0600

    v2.0.0 initial commit
    Use serialized master list
    Eliminate purge requirement
    Record messages in channel to chain

--- BestGirl/Program.fs
+++ BestGirl/Program.fs
@@ -6,6 +6,63 @@ open System
 open System.Threading.Tasks
 open System.Collections.Generic
 open Giraffe.Tasks
+open Newtonsoft.Json
+open MarkovGenerator
+
+// Right now all it does is record to chain if it's in the right channel
+module Monika =
+
+    let _client = new DiscordSocketClient()
+
+    let log (msg : LogMessage) =
+        msg.ToString() |> Console.WriteLine
+        Task.CompletedTask
+    
+    let generator = new Markov()
+    
+    let messageReceived (msg : SocketMessage) =
+        task {
+            let author = msg.Author.Username
+            let text = msg.Content
+            match msg.Channel.Name with
+                | "monika-training" ->
+                    generator.AddToChain text
+                | _ -> ()
+        }
+    
+    let mainasync = // Might have to eventually pass unit to make this work
+        task {
+
+            // Weird type stuff here prevents just saying `_client.add_Log log`
+            _client.add_Log (fun m -> log m)
+            _client.add_MessageReceived (fun m -> messageReceived m :> Task)
+            //_client.add_MessageReceived (fun m -> messageReceived m |> Extras.startAsPlainTask)
+
+            // Tokens
+            let DEVELOPMENT = "Development Token Here"
+            let token = DEVELOPMENT
+
+            do! _client.LoginAsync (TokenType.Bot, token)
+            do! _client.StartAsync ()
+
+            // Keeps it running
+            do! Task.Delay -1
+        }
+        
+    [<EntryPoint>]
+    let main argv = 
+        mainasync.GetAwaiter().GetResult()
+        0 // return an integer exit code
+
+(* I've commented out the real code
+namespace BestGirl
+
+open Discord
+open Discord.WebSocket
+open System
+open System.Threading.Tasks
+open System.Collections.Generic
+open Giraffe.Tasks
 
 module Extras =
     
@@ -61,17 +118,12 @@ module Monika =
                             let! throwaway_response = msg.Channel.SendMessageAsync response
                             () // return unit
                         | Other -> // Message doesn't match any command
+                            // Markov chain implementation will go here
                             if Extras.onein 10 then // One in ten chance of saying a voice line
-                                if Extras.onein 2 then // One in two chance of saying a preset line
-                                    let line = Lines.VoiceLines |> Extras.randomElement
-                                    let response = line.Replace("[player]", msg.Author.Mention)
-                                    let! throwaway_response = msg.Channel.SendMessageAsync response
-                                    () // return unit
-                                else // One in two chance of saying a Markov generated line
-                                    let line = Lines.VoiceLines |> Extras.randomElement
-                                    let response = line.Replace("[player]", msg.Author.Mention)
-                                    let! throwaway_response = msg.Channel.SendMessageAsync response
-                                    () // return unit
+                                let line = Lines.VoiceLines |> Extras.randomElement
+                                let response = line.Replace("[player]", msg.Author.Mention)
+                                let! throwaway_response = msg.Channel.SendMessageAsync response
+                                () // return unit
         }
     
     let mainasync = // Might have to eventually pass unit to make this work
@@ -85,8 +137,8 @@ module Monika =
             // // https://discordapp.com/oauth2/authorize?client_id=407367655830585344&scope=bot
 
             // Tokens
-            let DEVELOPMENT = "NDA3Mzk4NzkyMzc2MDkwNjI0.DWXvpw.JMkH9omQ8OXzcwpKrbjNRe3IQ8E"
-            let RELEASE = "NDA3MzY3NjU1ODMwNTg1MzQ0.DVOrsQ.QHFx1I6h8MTtkJiU_g7G7q0eaQY"
+            let DEVELOPMENT = "Development Token Here"
+            let RELEASE = "Release Token Here"
             let token = DEVELOPMENT
 
             do! _client.LoginAsync (TokenType.Bot, token)
@@ -100,3 +152,4 @@ module Monika =
     let main argv = 
         mainasync.GetAwaiter().GetResult()
         0 // return an integer exit code
+*)
---
+++ Markov/Markov.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using System.Text.RegularExpressions;
+
+namespace MarkovGenerator
+{
+    public class Markov
+    {
+        private string directory;
+        private string filename;
+        public List<Link> Chain { get; private set; }
+
+        public Markov(string _filename) // You can provide a custom filename
+        {
+            directory = Directory.GetCurrentDirectory();
+            filename = _filename;
+            Chain = this.ReadChain();
+
+        }
+        public Markov()
+        {
+            directory = Directory.GetCurrentDirectory();
+            filename = "markov.pdo";
+            Chain = this.ReadChain();
+        }
+
+        public List<Link> ReadChain()
+        {
+            if (File.Exists(filename))
+            {
+                var contents = File.ReadAllText(filename);
+                return JsonConvert.DeserializeObject<List<Link>>(contents);
+            }
+            else
+            {
+                return new List<Link>();
+            }
+        }
+
+        public void WriteChain()
+        {
+            var contents = JsonConvert.SerializeObject(Chain, Formatting.Indented);
+            File.WriteAllText(filename, contents);
+        }
+
+        public void AddToChain(Link link) // Adds the link to the chain, then serializes to file
+        {
+            if (link.Word != String.Empty)
+            {
+                var duplicates = Chain.Where(x => x.Word == link.Word).ToList();
+                if (duplicates.Count > 0)
+                {
+                    var i = Chain.IndexOf(duplicates[0]);
+                    Chain[i].AddBefore(link.Before);
+                    Chain[i].AddAfter(link.After);
+                }
+                else
+                {
+                    Chain.Add(link);
+                }
+                WriteChain();
+            }
+        }
+
+        public void AddToChain(string s)
+        {
+            var lower = s.ToLower();
+            var cleancopy = Regex.Replace(lower, "[^a-z ]+", "", RegexOptions.Compiled);
+            var words = cleancopy.Split(' ');
+            if (words.Length > 2)
+            {
+                var first = new Link(words.First());
+                first.AddAfter(words[1]);
+                AddToChain(first);
+                for (int i = 1; i < words.Length - 1; i++)
+                {
+                    var link = new Link(words[i], words[i - 1], words[i + 1]);
+                    AddToChain(link);
+                }
+                var last = new Link(words.Last());
+                last.AddBefore(words[words.Length - 2]);
+                AddToChain(last);
+            }
+            else if (words.Length == 2)
+            {
+                var first = new Link(words.First());
+                first.AddAfter(words.Last());
+                AddToChain(first);
+            }
+        }
+
+        public void Purge()
+        {
+            List<Link> flagged = new List<Link>();
+            for (int i = 0; i < Chain.Count; i++)
+            {
+                var _word = Chain[i].Word;
+                var copy = new List<Link>(Chain);
+                copy.RemoveAt(i);
+                List<Link> duplinks = copy.Where(x => x.Word == _word).ToList();
+                foreach (Link duplicate in duplinks)
+                {
+                    flagged.Add(duplicate);
+                    Chain[i].AddBefore(duplicate.Before);
+                    Chain[i].AddAfter(duplicate.After);
+                    Chain[i].Frequency++;
+                }
+            }
+
+            foreach (Link garbage in flagged)
+            {
+                Chain.Remove(garbage);
+            }
+            WriteChain();
+        }
+
+        public void SlowPurge()
+        {
+            List<Link> flagged = new List<Link>();
+            for (int i = 0; i < Chain.Count; i++)
+            {
+                foreach (Link duplink in Chain)
+                {
+                    // Ensure it's a duplicate (aka same word but not same index)
+                    if ((Chain.IndexOf(duplink) != i) && duplink.Word == Chain[i].Word)
+                    {
+                        flagged.Add(duplink);
+                        Chain[i].AddBefore(duplink.Before);
+                        Chain[i].AddAfter(duplink.After);
+                        Chain[i].Frequency++;
+                    }
+                }
+            }
+
+            foreach (Link garbage in flagged)
+            {
+                Chain.Remove(garbage);
+            }
+        }
+
+        public void PrintChain()
+        {
+            foreach (Link link in Chain)
+            {
+                Console.WriteLine(link.Word);
+            }
+        }
+
+        public String Generate(int length) // Generate based on given number of words and random starting word
+        {
+            Random randy = new Random();
+            string _word = Chain[randy.Next(0, Chain.Count)].Word;
+            string genChain = String.Empty;
+            for (int i = 0; i < length; i++) // You originally had two layers of this i think?
+            {
+                var links = Chain.Where(x => x.Word == _word).ToList();
+                foreach (Link lnk in links)
+                {
+                    genChain += _word + " ";
+                    _word = lnk.RandomAfter();
+                }
+            }
+            return genChain;
+        }
+        // TODO add generator based on starting word input (with and without length parameter)
+
+        // TODO add generator based on starting word input & number of sentences (with natural length through detecting punctuation)
+
+    }
+}
