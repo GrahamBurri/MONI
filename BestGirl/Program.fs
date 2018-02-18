906b4e54ea809894e35f2de86b33fb6bdd08f550
Devin <devin-lynch@uiowa.edu>
2018-02-17 19:57:51 -0600

    Restore functionality

--- BestGirl/Program.fs
+++ BestGirl/Program.fs
@@ -5,64 +5,10 @@ open Discord.WebSocket
 open System
 open System.Threading.Tasks
 open System.Collections.Generic
+open System.IO
 open Giraffe.Tasks
 open Newtonsoft.Json
 open MarkovGenerator
-
-// Right now all it does is record to chain if it's in the right channel
-module Monika =
-
-    let _client = new DiscordSocketClient()
-
-    let log (msg : LogMessage) =
-        msg.ToString() |> Console.WriteLine
-        Task.CompletedTask
-    
-    let generator = new Markov()
-    
-    let messageReceived (msg : SocketMessage) =
-        task {
-            let author = msg.Author.Username
-            let text = msg.Content
-            match msg.Channel.Name with
-                | "monika-training" ->
-                    generator.AddToChain text
-                | _ -> ()
-        }
-    
-    let mainasync = // Might have to eventually pass unit to make this work
-        task {
-
-            // Weird type stuff here prevents just saying `_client.add_Log log`
-            _client.add_Log (fun m -> log m)
-            _client.add_MessageReceived (fun m -> messageReceived m :> Task)
-            //_client.add_MessageReceived (fun m -> messageReceived m |> Extras.startAsPlainTask)
-
-            // Tokens
-            let DEVELOPMENT = "Development Token Here"
-            let token = DEVELOPMENT
-
-            do! _client.LoginAsync (TokenType.Bot, token)
-            do! _client.StartAsync ()
-
-            // Keeps it running
-            do! Task.Delay -1
-        }
-        
-    [<EntryPoint>]
-    let main argv = 
-        mainasync.GetAwaiter().GetResult()
-        0 // return an integer exit code
-
-(* I've commented out the real code
-namespace BestGirl
-
-open Discord
-open Discord.WebSocket
-open System
-open System.Threading.Tasks
-open System.Collections.Generic
-open Giraffe.Tasks
 
 module Extras =
     
@@ -75,55 +21,55 @@ module Extras =
     // Returns true or false based on specified odds
     let onein x = rng.Next(0, x) = 1
 
-    // Runs an async { } workflow as a Task
-    // let inline startAsPlainTask (work : Async<unit>) = Task.Factory.StartNew(fun () -> work |> Async.RunSynchronously)
-   
-
+// Right now all it does is record to chain if it's in the right channel
 module Monika =
-    
-    let (|Say|Monika|Delete|Other|) (txt : string) =
-        if txt.ToLower().Contains "<@407367655830585344> say" then Say
-        else if txt.ToLower().Contains "<@407367655830585344>" then Monika
-        else if txt.ToLower().StartsWith "delete" then Delete
-        else Other
 
     let _client = new DiscordSocketClient()
 
     let log (msg : LogMessage) =
         msg.ToString() |> Console.WriteLine
         Task.CompletedTask
+
+    let tagged (msg : SocketMessage) =
+        let mutable _tagged = false
+        for usr in msg.MentionedUsers do
+            if usr.Username = "Monika" || usr.Username = "Monika Dev" then
+                _tagged <- true
+        _tagged
+    
+    let generator = new Markov()
     
     let messageReceived (msg : SocketMessage) =
         task {
             let author = msg.Author.Username
             let text = msg.Content
-            if text.StartsWith ";" then ()
-            match msg.Author.Username with
-                | "Monika" -> ()
-                | usr ->
-                    match text with
-                        | Say ->
-                            let monikasay = "<@407367655830585344> say"
-                            let cutoff = text.IndexOf monikasay + monikasay.Length + 1
-                            let response = text.Substring cutoff
-                            let! throwaway_response = msg.Channel.SendMessageAsync response
-                            () // return unit
-                        | Monika ->
-                            let line = Lines.VoiceLines |> Extras.randomElement
-                            let response = line.Replace("[player]", msg.Author.Mention)
-                            let! throwaway_response = msg.Channel.SendMessageAsync response
-                            () // return unit
-                        | Delete ->
-                            let response = text.Substring 7 + ".chr deleted"
-                            let! throwaway_response = msg.Channel.SendMessageAsync response
-                            () // return unit
-                        | Other -> // Message doesn't match any command
-                            // Markov chain implementation will go here
-                            if Extras.onein 10 then // One in ten chance of saying a voice line
-                                let line = Lines.VoiceLines |> Extras.randomElement
-                                let response = line.Replace("[player]", msg.Author.Mention)
-                                let! throwaway_response = msg.Channel.SendMessageAsync response
-                                () // return unit
+            
+            // Only process other users' messages
+            if author <> "Monika" && author <> "Monika Dev" then
+                if tagged msg then
+                    if text.Contains " say " then
+                        let response = text.Substring (text.IndexOf "say" + 4)
+                        let! ignore_response = msg.Channel.SendMessageAsync response
+                        () // Do Nothing
+                    else if text.Contains " markov " then
+                        let ss = text.Substring (text.IndexOf "markov" + 7)
+                        if ss |> Seq.forall Char.IsDigit then // If it's a valid number
+                            let i = Convert.ToInt32 ss
+                            let response = generator.Generate i
+                            let! ignore_response = msg.Channel.SendMessageAsync response
+                            () // Do Nothing
+                    else
+                        let line = Lines.VoiceLines |> Extras.randomElement
+                        let response = line.Replace("[player]", msg.Author.Mention)
+                        let! ignore_response = msg.Channel.SendMessageAsync response
+                        () // Do Nothing
+                else if text.StartsWith "delete" then
+                    let character = text.Substring 7
+                    let response = sprintf "%s.chr deleted" character
+                    let! ignore_response= msg.Channel.SendMessageAsync response
+                    () // Do Nothing
+                else if msg.Channel.Name.StartsWith "markov-" then
+                    generator.AddToChain text // Record the message
         }
     
     let mainasync = // Might have to eventually pass unit to make this work
@@ -134,11 +80,8 @@ module Monika =
             _client.add_MessageReceived (fun m -> messageReceived m :> Task)
             //_client.add_MessageReceived (fun m -> messageReceived m |> Extras.startAsPlainTask)
 
-            // // https://discordapp.com/oauth2/authorize?client_id=407367655830585344&scope=bot
-
             // Tokens
-            let DEVELOPMENT = "Development Token Here"
-            let RELEASE = "Release Token Here"
+            let DEVELOPMENT = "Token Here"
             let token = DEVELOPMENT
 
             do! _client.LoginAsync (TokenType.Bot, token)
@@ -152,4 +95,3 @@ module Monika =
     let main argv = 
         mainasync.GetAwaiter().GetResult()
         0 // return an integer exit code
-*)
